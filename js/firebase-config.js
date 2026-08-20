1310ca2ccef40f1816da6556924826e233afcd4e
virtualsanil <lamasanil570@gmail.com>
2026-08-20 20:35:40 +0545

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,20 +1,13 @@
-// ========================================
-// FIREBASE CONFIG
-// ========================================
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import { getAuth } from
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import { getFirestore } from
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-
-// ========================================
-// YOUR FIREBASE CONFIG
-// ========================================
 
 const firebaseConfig = {
   apiKey: "AIzaSyBFvPT0nS3k-d5XHM-aOGqfrL98TzvjWF8",
@@ -27,28 +20,10 @@ const firebaseConfig = {
 };
 
 
-// ========================================
-// INITIALIZE FIREBASE
-// ========================================
-
 const app = initializeApp(firebaseConfig);
 
 
-// ========================================
-// FIREBASE SERVICES
-// ========================================
+// Export both Auth and Firestore
+export const auth = getAuth(app);
 
-const auth = getAuth(app);
-
-const db = getFirestore(app);
-
-
-// ========================================
-// EXPORT
-// ========================================
-
-export {
-    app,
-    auth,
-    db
-};
+export const db = getFirestore(app);
